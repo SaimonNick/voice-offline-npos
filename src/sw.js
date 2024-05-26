8d7f3fb198374621b7daf329aba534fb8aa45dfd
Saimon <rasstdx@gmail.com>
2024-05-26 20:21:46 +0300

    [1.1] - Добавлена озвучка фраз

--- src/sw.js
+++ src/sw.js
@@ -1,29 +1,12 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.management.getAll(function(extensions) {
-        let hasExtension = extensions.some(ext => ext.id === 'oobnbmnbdiollepahbognnihpaomcdpb');
-        chrome.declarativeNetRequest.updateDynamicRules({
-                        addRules: [{
-                            id: 1,
-                            priority: 1,
-                            action: {
-                                type: "redirect",
-                                redirect: {
-                                    regexSubstitution: chrome.runtime.getURL('speech/\\1')
-                                }
-                            },
-                            condition: {
-                                regexFilter: "^.*/speech/oksana/(\\d+\\.mp3)$",
-                                resourceTypes: ["media"]
-                            }
-                        }],
-                        removeRuleIds: [1]
-                    });
+chrome.runtime.onInstalled.addListener(function () {
 
+    chrome.management.getAll(function (extensions) {
+        let hasExtension = extensions.some(ext => ext.id === 'oobnbmnbdiollepahbognnihpaomcdpb');
         if (!hasExtension) {
             chrome.action.setBadgeText({
                 text: ``
             });
-            
+
         } else {
             console.log('Запуск прерван')
             chrome.action.setBadgeBackgroundColor({
@@ -34,4 +17,34 @@ chrome.runtime.onInstalled.addListener(function() {
             });
         }
     });
+
+    chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: [1]
+    }, function () {
+        if (chrome.runtime.lastError) {
+            console.error('Error removing rule:', chrome.runtime.lastError);
+        } else {
+            chrome.declarativeNetRequest.updateDynamicRules({
+                addRules: [{
+                    id: 1,
+                    priority: 1,
+                    action: {
+                        type: "redirect",
+                        redirect: {
+                            regexSubstitution: chrome.runtime.getURL('speech/\\1')
+                        }
+                    },
+                    condition: {
+                        regexFilter: "^.*/speech/maxim/(.*\\.mp3)$",
+                        resourceTypes: ["media"]
+                    }
+                }]
+            }, function () {
+                if (chrome.runtime.lastError) {
+                    console.error(chrome.runtime.lastError);
+                }
+            });
+        }
+    });
+
 });
